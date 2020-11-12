7c6fa1bd9f8ce0724ba1fab81ac5a716145cbe7b
Shamim Bin Nur <sbnsbn00@gmail.com>
2020-11-12 22:53:04 +0600

    Deploy git add .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Cards from './components/Cards/Cards'
+import Charts from './components/Chart/Chart'
+import CountryPicker from './components/CountryPicker/CountryPicker'
+import {fetchData} from './api'
+import ImageCorona from './images/image.png';
+import {Footer} from './components/Footer';
+import styles from './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+    state = {
+        data : {},
+        country : '',
+    }
+
+    handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country);
+        this.setState({
+            data : fetchedData,
+            country : country,
+        })
+
+    }
+
+    async componentDidMount(){
+        const fetchedData = await fetchData();
+        this.setState(
+            {
+                data : fetchedData,
+            }
+        )
+    }
+
+    
+    render(){
+        const {data, country} = this.state
+
+        return(
+            <div className = {styles.container}>
+                <img className={styles.image} src = {ImageCorona} alt={'Covid-19'} />
+                <Cards data = {data}/>
+                <CountryPicker handleCountryChange ={this.handleCountryChange}  />
+                <Charts data={data} country={country}/>
+                <Footer/>
+                
+                
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
